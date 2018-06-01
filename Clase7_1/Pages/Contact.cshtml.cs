d803c6311d667fcc4a0a73c927ab3b4a2a8b9bb4
alejo <alejo@LAPTOP-5SQL04DU>
2018-06-01 14:46:54 -0300

    cambios

---
+++ Clase7_1/Pages/Contact.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clase7_1.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            var fechaDeHoy = DateTime.Today;
+            var fechaAComparar = new DateTime(2018, 04, 15);
+
+            if (fechaDeHoy > fechaAComparar)
+            {
+                Message = "La fecha de hoy es mayor a" + fechaAComparar.ToLongDateString();
+
+            }
+            else
+            {
+                Message = "La fecha de hoy es menor a " + fechaAComparar.ToLongDateString();
+            }            
+        }
+    }
+}
---
+++ Clase7_1/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clase7_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        public double? resultado;
+
+        public void OnGet(double numero1,string operacion, double numero2)
+        {
+           
+            if (numero1 != 0 || numero2 != 0)
+            {
+                switch (operacion)
+                {
+                    case "suma": resultado=(numero1 + numero2); break;
+                    case "resta": resultado=(numero1 - numero2); break;
+                    case "multi": resultado=(numero1 * numero1); break;
+                    case "divi": resultado=(numero1 / numero2); break;
+                }
+            }
+
+        }
+
+        public ActionResult OnPost()
+        {
+            //Obtiene los valores del formulario, segun name
+            var n1 =Request.Form["numero1"];
+            var n2 =Request.Form["numero2"];
+            var opc = Request.Form["operacion"];
+            //Redirecciona Al GET con los parametros que enviamos
+            return Redirect("Index?numero1=" + n1 +"&operacion="+ opc +"&numero2=" + n2);
+
+        }
+    }
+}
